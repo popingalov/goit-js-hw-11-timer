3a26f398493bb7d2f7225c0fe3ce5ae6e1ea18fd
Oleksandr Balitskiy <popingalov@gmail.com>
2021-09-21 21:49:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class CountdownTimer {
       const newYearsDate = new Date(this.targetDate);
       const totalSecond = (newYearsDate - currentDate) / 1000;
 
-      const time = getTimeComponents(totalSecond);
+      const time = this.getTimeComponents(totalSecond);
       const { days, hours, mins, secs } = time;
 
       this.refs.fieldDays.textContent = days;
@@ -26,24 +26,26 @@ class CountdownTimer {
       this.refs.fieldSecs.textContent = secs;
     }, 1000);
   };
-}
-
-function getTimeComponents(time) {
-  let days = Math.floor(time / 3600 / 24);
-  let hours = Math.floor((time / 3600) % 24);
-  hours = String(hours).padStart(2, '0');
-  let mins = Math.floor((time / 60) % 60);
-  mins = String(mins).padStart(2, '0');
-  let secs = Math.floor(time % 60);
-  secs = String(secs).padStart(2, '0');
-  return { days, hours, mins, secs };
+  getTimeComponents = time => {
+    let days = Math.floor(time / 3600 / 24);
+    let hours = Math.floor((time / 3600) % 24);
+    hours = String(hours).padStart(2, '0');
+    let mins = Math.floor((time / 60) % 60);
+    mins = String(mins).padStart(2, '0');
+    let secs = Math.floor(time % 60);
+    secs = String(secs).padStart(2, '0');
+    return { days, hours, mins, secs };
+  };
+  autoStart = (() => {
+    this.start();
+  })();
 }
 
 const timer1 = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Dec 31, 2021'),
 });
+/* timer1.start(); */
 
-timer1.start();
 /////
 ////
